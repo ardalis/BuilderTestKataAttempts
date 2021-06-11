442e92ffa441a92413554602d2d41ff14499d4e9
Steve Smith <steve@kentsmiths.com>
2021-06-11 15:56:17 -0400

    Initial version

---
+++ 2021-06-11/src/BuilderTestSample/Tests/TestBuilders/OrderBuilder.cs
@@ -0,0 +1,33 @@
+﻿using BuilderTestSample.Model;
+
+namespace BuilderTestSample.Tests.TestBuilders
+{
+    /// <summary>
+    /// Reference: https://ardalis.com/improve-tests-with-the-builder-pattern-for-test-data
+    /// </summary>
+    public class OrderBuilder
+    {
+        private Order _order = new Order();
+
+        public OrderBuilder WithId(int id)
+        {
+            _order.Id = id;
+            return this;
+        }
+        public OrderBuilder WithTestValues()
+        {
+            _order.TotalAmount = 100m;
+
+            // TODO: replace next lines with a CustomerBuilder you create
+            // _order.Customer = new Customer();
+            // _order.Customer.HomeAddress = new Address();
+
+            return this;
+        }
+
+        public Order Build()
+        {
+            return _order;
+        }
+    }
+}
